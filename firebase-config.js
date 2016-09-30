bc841ca4191080d4f07cdd4e8722039448073795
JackBrasesco <jacbras@nuevaschool.org>
2016-09-30 08:39:36 -0700

    Classadoo autocommit

--- firebase-config.js
+++ firebase-config.js
@@ -10,7 +10,7 @@ var store = firebase.database().ref("challenge")
 
 function addMessage() {
   console.log("Adding message");
-  store.child("messages").push({message: "david sucks"})	
+  store.child("messages").push({message: "zach is lame"})	
 }
 
 function readMessages(callback) {
